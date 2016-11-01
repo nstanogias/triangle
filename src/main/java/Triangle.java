30a37897fc0f5da37a06d539284ab01965160a9a
Nikos Stanogias <niksta@sics.se>
2016-11-01 18:39:27 +0200

    finished challenge

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -3,7 +3,26 @@
  */
 public class Triangle {
 
-    public TriangleType triangleType(int aSide, int bSide, int cSide) {
-        return null;
+    public static TriangleType triangleType(int aSide, int bSide, int cSide) {
+        if(!isValidTriangle(aSide, bSide, cSide)) return TriangleType.INVALID;
+        if(aSide == bSide && bSide == cSide) return TriangleType.EQUILATERAL;
+        if(aSide == bSide || bSide == cSide || cSide == aSide) return TriangleType.ISOSCELES;
+
+        return TriangleType.SCALENE;
+    }
+
+
+    public static boolean isValidTriangle(int sideOne, int sideTwo, int sideThree) {
+        if (sideOne > sideTwo + sideThree ||
+                sideTwo > sideThree + sideOne ||
+                sideThree > sideOne + sideTwo) {
+            return false;
+        }
+        else if (sideOne <= 0 || sideTwo <= 0 || sideThree <= 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
--- src/test/java/TriangleTypeTest.java
+++ src/test/java/TriangleTypeTest.java
@@ -1,27 +1,31 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by nickstanogias on 01/11/16.
  */
 public class TriangleTypeTest {
 
     @Test
-    public void shouldFailWithNegativeSide(){
-
+    public void shouldReturnInvalidWhenCriteriaNotMet(){
+        assertEquals(TriangleType.INVALID, Triangle.triangleType(-1, 5, 5));
+        assertEquals(TriangleType.INVALID, Triangle.triangleType(0, 5, 5));
+        assertEquals(TriangleType.INVALID, Triangle.triangleType(1, 5, 7));
     }
 
     @Test
     public void shouldReturnScalene() {
-
+        assertEquals(TriangleType.SCALENE, Triangle.triangleType(3, 4, 5));
     }
 
     @Test
     public void shouldReturnIsosceles() {
-
+        assertEquals(TriangleType.ISOSCELES, Triangle.triangleType(5, 5, 10));
     }
 
     @Test
     public void shouldReturnEquilateral() {
-
+        assertEquals(TriangleType.EQUILATERAL, Triangle.triangleType(5, 5, 5));
     }
 }
